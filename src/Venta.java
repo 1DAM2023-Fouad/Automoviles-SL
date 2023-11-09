9a1babf630d6ed77416f09cb65ef6405224a1f1b
Fouad <fouadaharchi1@gmail.com>
2023-11-09 15:47:48 +0100

    Ejercicio Automoviles Con los códigos, me falta el main

--- src/Venta.java
+++ src/Venta.java
@@ -1,2 +1,57 @@
+import java.util.ArrayList;
+
 public class Venta {
+    private String codigoVenta;
+    private String fechaVenta;
+    private String formaPago;
+    private String precioVenta;
+    private ArrayList<Cliente> clientes;
+    private Vehiculo vehiculo;
+
+    public String getCodigoVenta() {
+        return codigoVenta;
+    }
+
+    public void setCodigoVenta(String codigoVenta) {
+        this.codigoVenta = codigoVenta;
+    }
+
+    public String getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(String fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    public String getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(String precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(ArrayList<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
 }
